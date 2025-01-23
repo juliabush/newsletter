ad22fc889b4ceec638929a744c72aefe69e40996
juliabush <j.elizabethbush@gmail.com>
2025-01-23 22:07:58 +0100

    Design nearly 100% finished, just have top display user email

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
+const errorMessage = document.getElementById('error');
+const signUpForm = document.getElementById('card-1');
+const successMessage = document.getElementById('card-2');
+const refreshButton = document.getElementById('refresh');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    let messages = []
+    const regex = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/;
+    if (!regex.test(email.value)) {
+        messages.push('Valid Email Required')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorMessage.innerText = messages.join(', ')
+        errorMessage.classList.toggle('red-text');
+        email.classList.toggle('red');
+    } 
+    if (messages.length === 0) {
+        e.preventDefault()
+        signUpForm.classList.add('hidden');
+        successMessage.classList.remove('hidden');
+    }
+})
+refreshButton.addEventListener('click', function() {
+    location.reload();
 })
